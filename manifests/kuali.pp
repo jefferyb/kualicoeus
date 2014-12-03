72d7615a846362e1cd74305112ecb7e5903f6a6f
Jeffery Bagirimvano <jeffery.rukundo@gmail.com>
2014-12-03 00:40:34 +0000

    A quick bug fix

--- manifests/kuali.pp
+++ manifests/kuali.pp
@@ -3,13 +3,24 @@
 #
 
 class kualicoeus::kuali {
-  require ::kualicoeus::config::folders
+  if $kualicoeus::setup_tomcat == true {
+    require ::kualicoeus::config::folders
 
-  file { 'kc-config.xml File':
-    ensure  => 'present',
-    path    => "${kualicoeus::kc_config_home}/kc-config.xml",
-    content => template('kualicoeus/kc-config.xml.erb'),
-    notify  => Tomcat::Service['default'],
-    mode    => '0664';
+    file { 'kc-config.xml File':
+      ensure  => 'present',
+      path    => "${kualicoeus::kc_config_home}/kc-config.xml",
+      content => template('kualicoeus/kc-config.xml.erb'),
+      notify  => Tomcat::Service['default'],
+      mode    => '0664';
+    }
+  } else {
+    require ::kualicoeus::config::folders
+
+    file { 'kc-config.xml File':
+      ensure  => 'present',
+      path    => "${kualicoeus::kc_config_home}/kc-config.xml",
+      content => template('kualicoeus/kc-config.xml.erb'),
+      mode    => '0664';
+    }
   }
 }
